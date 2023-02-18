42d585203b8cece6956a8244baeb4ecba19d99bb
charles <chleighton1@gmail.com>
2023-02-18 17:59:54 -0500

    Change to postgres

--- taskninja/config.py
+++ taskninja/config.py
@@ -2,10 +2,10 @@ import os
 
 
 class Config:
-    SECRET_KEY = '3324aabb83f14c3cf5450caef74e8770'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    MAIL_USERNAME = 'taskninja11@gmail.com'
-    MAIL_PASSWORD = 'msgpgorygwthudlz'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
