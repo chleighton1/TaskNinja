361cc11713744a3a9dfab66cde4e96409be9a5af
charles <chleighton1@gmail.com>
2023-02-19 16:25:23 -0500

    python 3.8

--- taskninja/config.py
+++ taskninja/config.py
@@ -11,13 +11,3 @@ class Config:
     MAIL_PORT = 587
     MAIL_USE_TLS = True
 
-
-    # SECRET_KEY = '3324aabb83f14c3cf5450caef74e8770'
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    # MAIL_USERNAME = 'taskninja11@gmail.com'
-    # MAIL_PASSWORD = 'msgpgorygwthudlz'
-
-    # SECRET_KEY = os.environ.get('SECRET_KEY')
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
